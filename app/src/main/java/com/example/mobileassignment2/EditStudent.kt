65dfb485fe898439bd939b70e9d866838adc4ada
inbal <inbalbarak123@gmail.com>
2025-01-01 17:48:43 +0200

    finish second part of mission

--- app/src/main/java/com/example/mobileassignment2/EditStudent.kt
+++ app/src/main/java/com/example/mobileassignment2/EditStudent.kt
@@ -1,10 +1,17 @@
 package com.example.mobileassignment2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mobileassignment2.model.Model
+import com.example.mobileassignment2.model.Student
 
 class EditStudent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,69 @@ class EditStudent : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backToolbarIcon: ImageView = findViewById(R.id.back_button);
+
+        backToolbarIcon.setOnClickListener {
+            finish()
+        }
+
+        val students = Model.shared.students
+
+        val position = intent.getIntExtra("position", -1)
+
+        val saveButton: Button = findViewById(R.id.edit_student_activity_save_button);
+        val cancelButton: Button = findViewById(R.id.edit_student_activity_cancel_button);
+        val deleteButton: Button = findViewById(R.id.edit_student_activity_delete_button);
+
+        val nameEditText: EditText = findViewById(R.id.edit_student_activity_name_edit_text);
+        val idEditText: EditText = findViewById(R.id.edit_student_activity_id_edit_text);
+        val phoneEditText: EditText = findViewById(R.id.edit_student_activity_phone_edit_text);
+        val addressEditText: EditText = findViewById(R.id.edit_student_activity_address_edit_text);
+        val checkedCheckbox: CheckBox = findViewById(R.id.edit_student_activity_checked_checkbox);
+
+        if(position != -1) {
+            var student = students[position]
+
+            nameEditText.setText(student.name)
+            idEditText.setText(student.id)
+            phoneEditText.setText(student.phone)
+            addressEditText.setText(student.address)
+            checkedCheckbox.isChecked = student.isChecked
+        }
+
+
+        cancelButton.setOnClickListener {
+            finish()
+        }
+
+        deleteButton.setOnClickListener {
+            if(position != -1) {
+                students.removeAt(position)
+            }
+
+            val intent = Intent(this, StudentsList::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
+
+        saveButton.setOnClickListener {
+            if(position != -1) {
+                val student = Student(
+                    name = nameEditText.text.toString(),
+                    id = idEditText.text.toString(),
+                    phone = phoneEditText.text.toString(),
+                    address = addressEditText.text.toString(),
+                    isChecked = checkedCheckbox.isChecked
+                )
+
+                students[position] = student
+            }
+
+            finish()
+
         }
     }
 }
--- app/src/main/java/com/example/mobileassignment2/StudentDetails.kt
+++ app/src/main/java/com/example/mobileassignment2/StudentDetails.kt
@@ -1,10 +1,17 @@
 package com.example.mobileassignment2
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mobileassignment2.model.Model
 
 class StudentDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,43 @@ class StudentDetails : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backToolbarIcon: ImageView = findViewById(R.id.back_button);
+
+        backToolbarIcon.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val position = intent.getIntExtra("position", -1)
+
+        val editStudentButton: Button = findViewById(R.id.student_details_activity_edit_button);
+        editStudentButton.setOnClickListener {
+            val intent = Intent(this, EditStudent::class.java)
+            intent.putExtra("position", position)
+            startActivity(intent)
+        }
+
+        if(position != -1) {
+            val student = Model.shared.students[position]
+            Log.d("TAG", "print student now: ${student.toString()}")
+
+
+            val nameTextView: TextView = findViewById(R.id.student_details_activity_name_text_view)
+            val idTextView: TextView = findViewById(R.id.student_details_activity_id_text_view)
+            val phoneTextView: TextView = findViewById(R.id.student_details_activity_phone_text_view)
+            val addressTextView: TextView = findViewById(R.id.student_details_activity_address_text_view)
+            val checkedCheckbox: CheckBox = findViewById(R.id.student_details_activity_checked_checkbox)
+
+            nameTextView.text = student.name
+            idTextView.text = student.id
+            phoneTextView.text = student.phone
+            addressTextView.text = student.address
+            checkedCheckbox.isChecked = student.isChecked
         }
     }
 }
